8dadc8ebd155433b8a033d27d15043420d9c3d99
drashtilimbasiya <drashtidhirubhailimbasiya@gmail.com>
2026-05-01 21:37:20 +0530

    Pattern-3
    Pattern of 1 to 5 numbers start from reverse . Like 1,2,3,4,5 at first line

---
+++ Pattern_3.php
@@ -0,0 +1,11 @@
+<?php
+echo "Pattern of 1 to 5 numbers start from reverse \n";
+for($i=5; $i>=1; $i--)
+    {
+    for($j=1; $j<=$i; $j++)
+        {
+        echo $j;
+        }
+    echo "\n";
+    }
+?>
